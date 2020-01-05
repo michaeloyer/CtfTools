aa6433915ca707e169f5ec953dfa7e7dd802a594
Michael Oyer <code@michaeloyer.com>
2020-01-05 16:07:11 -0500

    Add Binary Methods

---
+++ CtfTools/Binary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CtfTools
+{
+    public static class Binary
+    {
+        public static int BinaryToByte(this IEnumerable<int> bits) => Convert.ToByte(string.Concat(bits), 2);
+        public static int BinaryToByte(this string bits) => Convert.ToByte(bits, 2);
+    }
+}
