a34f349ef855459a2b900a0a6863b892f4ebe39d
Michael Oyer <code@michaeloyer.com>
2020-01-05 18:57:34 -0500

    Add Clipboard Functionality

---
+++ CtfTools/Clipboard.cs
@@ -0,0 +1,11 @@
+﻿namespace CtfTools
+{
+    public static class Clipboard
+    {
+        public static void ToClipboard(this string text) =>
+            TextCopy.Clipboard.SetText(text);
+
+        public static string GetClipboard() =>
+            TextCopy.Clipboard.GetText() ?? string.Empty;
+    }
+}
