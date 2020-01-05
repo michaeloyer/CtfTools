d90271a00a4d2f754c303d7b304dd118288a1459
Michael Oyer <code@michaeloyer.com>
2020-01-05 13:57:07 -0500

    Rename to FileSystem

--- CtfTools/FileSystem.cs
+++ CtfTools/FileSystem.cs
@@ -1,19 +1,20 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 
 namespace CtfTools
 {
-    public static class File
+    public static class FileSystem
     {
         public static string ReadFile(string path) =>
             ReadFile(path, Encoding.UTF8);
 
         public static string ReadFile(string path, Encoding encoding) =>
-            System.IO.File.ReadAllText(path, encoding);
+            File.ReadAllText(path, encoding);
 
         public static void WriteFile(this string text, string path) =>
             WriteFile(text, path, Encoding.UTF8);
 
         public static void WriteFile(this string text, string path, Encoding encoding) =>
-            System.IO.File.WriteAllText(path, text, encoding);
+            File.WriteAllText(path, text, encoding);
     }
 }
