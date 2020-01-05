e01075666847515be0bf0a2cde0d07b7506f74bd
Michael Oyer <code@michaeloyer.com>
2020-01-05 16:08:06 -0500

    Add Striing Parser Extension Methods

--- CtfTools/StringExtensions.cs
+++ CtfTools/StringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Superpower;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -54,5 +55,11 @@ namespace CtfTools
         private static readonly Phonix.Soundex soundex = new Phonix.Soundex();
         public static string Soundex(this string text) => soundex.BuildKey(text);
         public static T FromJson<T>(this string text) => JsonConvert.DeserializeObject<T>(text);
+
+        public static T TextParse<TToken, T>(this string text, TextParser<T> parser) =>
+            parser.Parse(text);
+
+        public static T TokenParse<TToken, T>(this string text, Tokenizer<TToken> tokenizer, TokenListParser<TToken, T> tokenParser) =>
+            tokenParser.Parse(tokenizer.Tokenize(text));
     }
 }
