fac90c245f9093287856da635c0b444751641c92
Michael Oyer <code@michaeloyer.com>
2020-01-05 11:56:18 -0500

    Add Console Extension Methods

---
+++ CtfTools/ObjectExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CtfTools
+{
+    public static class ObjectExtensions
+    {
+        public static void ConsoleWrite(this object obj) => Console.Write(obj);
+        public static void ConsoleWriteLine(this object obj) => Console.WriteLine(obj);
+    }
+}
