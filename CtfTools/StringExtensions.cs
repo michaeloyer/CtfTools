25760fe7cded054b90e44bf692aa4623cce7d83e
Michael Oyer <code@michaeloyer.com>
2020-01-05 11:37:22 -0500

    Add Base64 String Extension Methods

---
+++ CtfTools/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace CtfTools
+{
+    public static class StringExtensions
+    {
+        public static string DecodeBase64(this string text) =>
+            DecodeBase64(text, Encoding.UTF8);
+
+        public static string DecodeBase64(this string text, Encoding encoding) =>
+            encoding.GetString(Convert.FromBase64String(text));
+
+        public static string EncodeBase64(this string text) =>
+            EncodeBase64(text, Encoding.UTF8);
+
+        public static string EncodeBase64(this string text, Encoding encoding) =>
+            Convert.ToBase64String(encoding.GetBytes(text));
+    }
+}
