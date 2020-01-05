440e3822f8140339e54ef4fe19a39fb68aacc39e
Michael Oyer <code@michaeloyer.com>
2020-01-05 11:49:40 -0500

    Add Regex String Extension Methods

--- CtfTools/StringExtensions.cs
+++ CtfTools/StringExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace CtfTools
 {
@@ -16,5 +19,25 @@ namespace CtfTools
 
         public static string EncodeBase64(this string text, Encoding encoding) =>
             Convert.ToBase64String(encoding.GetBytes(text));
+        public static string RegexMatch(this string text, string pattern) =>
+            RegexMatch(text, pattern, RegexOptions.None);
+
+        public static string RegexMatch(this string text, string pattern, RegexOptions options) =>
+            Regex.Match(text, pattern, options).Value;
+
+        public static IEnumerable<string> RegexMatches(this string text, string pattern) =>
+            RegexMatches(text, pattern, RegexOptions.None);
+
+        public static IEnumerable<string> RegexMatches(this string text, string pattern, RegexOptions options) =>
+            Regex.Matches(text, pattern, options).Cast<Match>().Select(m => m.Value);
+
+        public static string RegexReplace(this string text, string pattern, string replacement) =>
+            RegexReplace(text, pattern, replacement, RegexOptions.None);
+
+        public static string RegexReplace(this string text, string pattern, string replacement, RegexOptions options) =>
+            Regex.Replace(text, pattern, replacement, options);
+
+        public static IEnumerable<string> GetWords(this string text) =>
+            RegexMatches(text, @"\S+");
     }
 }
