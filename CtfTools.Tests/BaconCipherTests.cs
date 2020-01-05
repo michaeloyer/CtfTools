5c67ecce25413329bf264f93266df9dc5919784e
Michael Oyer <code@michaeloyer.com>
2020-01-05 12:48:08 -0500

    Add Bacon Cipher

---
+++ CtfTools.Tests/BaconCipherTests.cs
@@ -0,0 +1,91 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CtfTools.Tests
+{
+    [TestClass]
+    public class BaconCipherTests
+    {
+        [DataTestMethod]
+        [DataRow('A', 0)]
+        [DataRow('B', 1)]
+        [DataRow('Z', 23)]
+        public void NormalUsingValidCharacter_ReturnsNumber(char character, int number)
+        {
+            BaconCipher.Normal[character].Should().Be(number);
+        }
+
+        [DataTestMethod]
+        [DataRow('J')]
+        [DataRow('V')]
+        [DataRow('?')]
+        public void NormalUsingInvalidCharacter_ThrowsBaconCipherException(char invalidCharacter)
+        {
+            Assert.ThrowsException<BaconCipherException>(() =>
+                BaconCipher.Normal[invalidCharacter]
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow(0, 'A')]
+        [DataRow(1, 'B')]
+        [DataRow(23, 'Z')]
+        public void NormalUsingValidNumber_ReturnsCharacter(int number, char character)
+        {
+            BaconCipher.Normal[number].Should().Be(character);
+        }
+
+        [DataTestMethod]
+        [DataRow(24)]
+        [DataRow(25)]
+        [DataRow(26)]
+        [DataRow(27)]
+        public void NormalUsingInvalidNumber_ThrowsBaconCipherException(int invalidNumber)
+        {
+            Assert.ThrowsException<BaconCipherException>(() =>
+                BaconCipher.Normal[invalidNumber]
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow('A', 0)]
+        [DataRow('B', 1)]
+        [DataRow('J', 9)]
+        [DataRow('V', 21)]
+        [DataRow('Z', 25)]
+        public void FullUsingValidCharacter_ReturnsNumber(char character, int number)
+        {
+            BaconCipher.Full[character].Should().Be(number);
+        }
+
+        [DataTestMethod]
+        [DataRow('?')]
+        public void FullUsingInvalidCharacter_ThrowsBaconCipherException(char invalidCharacter)
+        {
+            Assert.ThrowsException<BaconCipherException>(() =>
+                BaconCipher.Full[invalidCharacter]
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow(0, 'A')]
+        [DataRow(1, 'B')]
+        [DataRow(9, 'J')]
+        [DataRow(21, 'V')]
+        [DataRow(25, 'Z')]
+        public void FullUsingValidNumber_ReturnsCharacter(int number, char character)
+        {
+            BaconCipher.Full[number].Should().Be(character);
+        }
+
+        [DataTestMethod]
+        [DataRow(26)]
+        [DataRow(27)]
+        public void FullUsingInvalidNumber_ThrowsBaconCipherException(int invalidNumber)
+        {
+            Assert.ThrowsException<BaconCipherException>(() =>
+                BaconCipher.Full[invalidNumber]
+            );
+        }
+    }
+}
---
+++ CtfTools/BaconCipher.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace CtfTools
+{
+    public class BaconCipher
+    {
+        private readonly Dictionary<char, int> CharToInt = new Dictionary<char, int>();
+        private readonly Dictionary<int, char> IntToChar = new Dictionary<int, char>();
+
+        private BaconCipher(IEnumerable<(char character, int number)> collection)
+        {
+            foreach (var (character, number) in collection)
+            {
+                CharToInt.Add(character, number);
+                IntToChar.Add(number, character);
+            }
+        }
+
+        /// <summary>
+        /// Bacon Cipher, Excludes 'J' and 'V'
+        /// </summary>
+        public static BaconCipher Normal = new BaconCipher(new[] {
+            ('A', 0),
+            ('B', 1),
+            ('C', 2),
+            ('D', 3),
+            ('E', 4),
+            ('F', 5),
+            ('G', 6),
+            ('H', 7),
+            ('I', 8),
+            ('K', 9),
+            ('L', 10),
+            ('M', 11),
+            ('N', 12),
+            ('O', 13),
+            ('P', 14),
+            ('Q', 15),
+            ('R', 16),
+            ('S', 17),
+            ('T', 18),
+            ('U', 19),
+            ('W', 20),
+            ('X', 21),
+            ('Y', 22),
+            ('Z', 23),
+        });
+
+
+        /// <summary>
+        /// Bacon Cipher Inclues 'J' and 'V'
+        /// </summary>
+        public static BaconCipher Full = new BaconCipher(new[] {
+            ('A', 0),
+            ('B', 1),
+            ('C', 2),
+            ('D', 3),
+            ('E', 4),
+            ('F', 5),
+            ('G', 6),
+            ('H', 7),
+            ('I', 8),
+            ('J', 9),
+            ('K', 10),
+            ('L', 11),
+            ('M', 12),
+            ('N', 13),
+            ('O', 14),
+            ('P', 15),
+            ('Q', 16),
+            ('R', 17),
+            ('S', 18),
+            ('T', 19),
+            ('U', 20),
+            ('V', 21),
+            ('W', 22),
+            ('X', 23),
+            ('Y', 24),
+            ('Z', 25),
+        });
+
+        public int this[char character]
+        {
+            get
+            {
+                var c = char.ToUpper(character);
+                switch (character)
+                {
+                    case 'J':
+                    case 'V':
+                        if (!CharToInt.ContainsKey(character))
+                            throw new BaconCipherException($"'J' and 'V' are not in Bacon Cipher, Consider using {nameof(BaconCipher)}.{nameof(Full)}");
+                        return CharToInt[character];
+                    default:
+                        if (!CharToInt.ContainsKey(character))
+                            throw new BaconCipherException($"Character not in Bacon Cipher: '{character}'");
+                        return CharToInt[character];
+                }
+            }
+        }
+
+        public char this[int number]
+        {
+            get
+            {
+                switch (number)
+                {
+                    case 24:
+                    case 25:
+                        if (!IntToChar.ContainsKey(number))
+                            throw new BaconCipherException($"'24' and '25' are not in Bacon Cipher, Consider using {nameof(BaconCipher)}.{nameof(Full)}");
+                        return IntToChar[number];
+                    default:
+                        if (!IntToChar.ContainsKey(number))
+                            throw new BaconCipherException($"Number not in Bacon Cipher: '{number}'");
+                        return IntToChar[number];
+                }
+            }
+        }
+    }
+
+    public class BaconCipherException : Exception
+    {
+        public BaconCipherException()
+        {
+        }
+
+        public BaconCipherException(string message) : base(message)
+        {
+        }
+
+        public BaconCipherException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected BaconCipherException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
