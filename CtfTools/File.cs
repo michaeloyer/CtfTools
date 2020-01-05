43f54da202c8f512dc16123dfec26ddf05482592
Michael Oyer <code@michaeloyer.com>
2020-01-05 11:56:09 -0500

    Add File Methods

---
+++ CtfTools/File.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace CtfTools
+{
+    public static class File
+    {
+        public static string ReadFile(string path) =>
+            ReadFile(path, Encoding.UTF8);
+
+        public static string ReadFile(string path, Encoding encoding) =>
+            System.IO.File.ReadAllText(path, encoding);
+
+        public static void WriteFile(this string text, string path) =>
+            WriteFile(text, path, Encoding.UTF8);
+
+        public static void WriteFile(this string text, string path, Encoding encoding) =>
+            System.IO.File.WriteAllText(path, text, encoding);
+    }
+}
