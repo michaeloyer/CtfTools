71bd52e2de68d12286ffda1409f806f995774cf2
Michael Oyer <code@michaeloyer.com>
2020-01-05 19:53:55 -0500

    Write to console on sending to clipboard

--- CtfTools/Clipboard.cs
+++ CtfTools/Clipboard.cs
@@ -1,9 +1,23 @@
-﻿namespace CtfTools
+﻿using System;
+
+namespace CtfTools
 {
     public static class Clipboard
     {
-        public static void ToClipboard(this string text) =>
+        public static void ToClipboard(this string text)
+        {
             TextCopy.Clipboard.SetText(text);
+
+            var previousColor = Console.ForegroundColor;
+            Console.Write($"Text sent to clipboard: ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"|");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(text);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"|");
+            Console.ForegroundColor = previousColor;
+        }
 
         public static string GetClipboard() =>
             TextCopy.Clipboard.GetText() ?? string.Empty;
