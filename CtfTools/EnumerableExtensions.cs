a01b173b73181814d4b509d984f0c7df51344d3c
Michael Oyer <code@michaeloyer.com>
2020-01-05 20:37:06 -0500

    Add ToFlag Method

---
+++ CtfTools/EnumerableExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace CtfTools
+{
+    public static class EnumerableExtensions
+    {
+        public static string ToFlag(this IEnumerable<object> collection, string prefix = "")
+        {
+            var builder = new StringBuilder();
+
+            using (var e = collection.GetEnumerator())
+            {
+                builder.Append(prefix);
+                e.MoveNext();
+                var part = e.Current.ToString();
+
+                if (part != prefix)
+                {
+                    builder.Append("-");
+                    builder.Append(part);
+                }
+
+                while (e.MoveNext())
+                {
+                    builder.Append("-");
+                    builder.Append(e.Current);
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
